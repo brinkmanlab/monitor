9eceaa592e107fb89982ee225ea084302dd324cc
Nolan Woods <nolan_w@sfu.ca>
2021-10-07 21:28:10 -0700

    add lambda permission

--- destinations/aws/main.tf
+++ destinations/aws/main.tf
@@ -146,3 +146,11 @@ resource "aws_cloudwatch_event_target" "monitor" {
     maximum_event_age_in_seconds = 60
   }
 }
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_monitor" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.monitor.function_name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.monitor.arn
+}
